b54e53b49e11bac4d23d2d754ce47bd7df56740e
Антон Новосельцев <raitfolt@gmail.com>
2022-12-13 13:35:39 +0500

    composition done

--- composition/main.go
+++ composition/main.go
@@ -6,6 +6,24 @@ import (
 )
 
 func main() {
+
+	products := map[string]store.ItemForSale{
+		"Kayak": store.NewBoat("Kayak", 279, 1, false),
+		"Ball":  store.NewProduct("Soccer Ball", "Soccer", 19.50),
+	}
+
+	for key, p := range products {
+		switch item := p.(type) {
+		case store.Describable:
+			fmt.Println(
+				"Name:", item.GetName(),
+				"Category:", item.GetCategory(),
+				"Price:", item.Price(0.2))
+		default:
+			fmt.Println("Key:", key, "Price:", p.Price(0.2))
+		}
+	}
+
 	kayak := store.NewProduct("Kayak", "Watersports", 279)
 
 	type OfferBundle struct {
