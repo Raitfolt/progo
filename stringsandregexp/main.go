969b99c6d3a95d5cbca487d2556f3f1ffd1118ee
Антон Новосельцев <raitfolt@gmail.com>
2022-12-16 15:58:01 +0500

    16.12.2022

--- stringsandregexp/main.go
+++ stringsandregexp/main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"unicode"
 )
-
-//525
 
 func main() {
 	product := "Kayak"
 
-	fmt.Println("Product:", product)
+	for _, char := range product {
+		fmt.Println(string(char), "Upper case:", unicode.IsUpper(char))
+	}
+
+	fmt.Println("Contains:", strings.Contains(product, "yak"))
+	fmt.Println("ContainsAny:", strings.ContainsAny(product, "abc"))
+	fmt.Println("ContainsRune:", strings.ContainsRune(product, 'K'))
+	fmt.Println("EqualFold:", strings.EqualFold(product, "KAYAK"))
+	fmt.Println("HasPrefix:", strings.HasPrefix(product, "Ka"))
+	fmt.Println("HasSuffix:", strings.HasSuffix(product, "yak"))
+
+	kayak := "KaYaK iS gOoD"
+	fmt.Println(kayak, "to lower:", strings.ToLower(kayak))
+	fmt.Println(kayak, "to upper:", strings.ToUpper(kayak))
+	fmt.Println(kayak, "title:", strings.Title(kayak))
+	fmt.Println(kayak, "to title:", strings.ToTitle(kayak))
+
 }
