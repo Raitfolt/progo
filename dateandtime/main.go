4548fbade3b30d854a19a57b6baa05ca77fe25ba
Антон Новосельцев <raitfolt@gmail.com>
2022-12-27 16:52:55 +0500

    27.12.2022

--- dateandtime/main.go
+++ dateandtime/main.go
@@ -12,38 +12,28 @@ func PrintTime(label string, t *time.Time) {
 	//Printfln("%s: Day: %v Month: %v Year: %v", label, t.Day(), t.Month(), t.Year())
 }
 
-func writeToChannel(channel chan<- string) {
+//648
 
-	Printfln("Waiting for initial duration...")
-	_ = <-time.After(time.Second * 2)
-	Printfln("Initial duration elapsed.")
+func writeToChannel(nameChannel chan<- string) {
 	names := []string{"Alice", "Bob", "Charlie", "Dora"}
-	for _, name := range names {
-		channel <- name
-		time.Sleep(time.Second * 3)
+
+	tickChannel := time.Tick(time.Second)
+	index := 0
+
+	for {
+		<-tickChannel
+		nameChannel <- names[index]
+		index++
+		if index == len(names) {
+			index = 0
+		}
 	}
-	close(channel)
 }
 
 func main() {
 	nameChannel := make(chan string)
 
 	go writeToChannel(nameChannel)
-
-	channelOpen := true
-	for channelOpen {
-		select {
-		case name, ok := <-nameChannel:
-			if !ok {
-				channelOpen = false
-				break
-			} else {
-				Printfln("Read name: %v", name)
-			}
-		case <-time.After(time.Second * 2):
-			Printfln("Timeout")
-		}
-	}
 
 	for name := range nameChannel {
 		Printfln("Read name: %v", name)
