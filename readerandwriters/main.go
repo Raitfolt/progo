a80d484f9675f359aac25258df25043bedacd691
Антон Новосельцев <raitfolt@gmail.com>
2022-12-29 15:08:53 +0500

    readerandwriters done

--- readerandwriters/main.go
+++ readerandwriters/main.go
@@ -37,19 +37,38 @@ func scanSingle(reader io.Reader, val interface{}) (int, error) {
 	return fmt.Fscan(reader, val)
 }
 
+func writeFormatted(writer io.Writer, template string, vals ...interface{}) {
+	fmt.Fprintf(writer, template, vals...)
+}
+
+func writeReplaced(writer io.Writer, str string, subs ...string) {
+	replacer := strings.NewReplacer(subs...)
+	replacer.WriteString(writer, str)
+}
+
 func main() {
 	reader := strings.NewReader("Kayak Watersports $279.00")
 
-	var name, category string
-	var price float64
-	scanTemplate := "%s %s $%f"
-
-	_, err := scanFromReader(reader, scanTemplate, &name, &category, &price)
-	if err != nil {
-		Printfln("Error: %v", err.Error())
-	} else {
-		Printfln("Name: %v", name)
-		Printfln("Category: %v", category)
-		Printfln("Price: %.2f", price)
+	for {
+		var str string
+		_, err := scanSingle(reader, &str)
+		if err != nil {
+			if err != io.EOF {
+				Printfln("Error: %v", err.Error())
+			}
+			break
+		}
+		Printfln("Value: %v", str)
 	}
+
+	var writer strings.Builder
+	template := "Name: %s, Category: %s, Price: $%.2f\n"
+	writeFormatted(&writer, template, "Kayak", "Watersports", float64(279))
+
+	fmt.Println(writer.String())
+
+	text := "It was a boat. A small boat."
+	subs := []string{"boat", "kayak", "small", "huge"}
+	writeReplaced(&writer, text, subs...)
+	fmt.Println(writer.String())
 }
