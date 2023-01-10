d1892cc75a82353814fc93f944c71e35e3d08f2a
Anton Novoseltcev <raitfolt@gmain.com>
2023-01-10 16:06:18 +0500

    files json save

--- files/main.go
+++ files/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"os"
-	"time"
 )
 
 func main() {
-	total := 0.0
+	cheapProducts := []Product{}
 	for _, p := range Products {
-		total += p.Price
+		if p.Price < 100 {
+			cheapProducts = append(cheapProducts, p)
+		}
 	}
-
-	dataStr := fmt.Sprintf("Time: %v, Total: $%.2f\n", time.Now().Format("Mon 15:04:05"), total)
-
-	err := os.WriteFile("output.txt", []byte(dataStr), 0666)
+	file, err := os.OpenFile("cheap.json", os.O_WRONLY|os.O_CREATE, 0666)
 	if err == nil {
-		fmt.Println("Output file created")
+		defer file.Close()
+		encoder := json.NewEncoder(file)
+		encoder.Encode(cheapProducts)
 	} else {
 		Printfln("Error: %v", err.Error())
 	}
