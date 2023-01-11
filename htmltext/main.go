db27139900ceb358b469f139bb640770617903a8
Anton Novoseltcev <raitfolt@gmain.com>
2023-01-11 16:21:29 +0500

    htmltext multi templates

--- htmltext/main.go
+++ htmltext/main.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	"os"
+	"text/template"
+)
+
 func main() {
-	for _, p := range Products {
-		Printfln("Product: %v, Category: %v, Price: $%.2f", p.Name, p.Category, p.Price)
+	t1, err1 := template.ParseFiles("templates/template.html")
+	t2, err2 := template.ParseFiles("templates/extras.html")
+	if err1 == nil && err2 == nil {
+		t1.Execute(os.Stdout, &Kayak)
+		os.Stdout.WriteString("\n")
+		t2.Execute(os.Stdout, &Kayak)
+		os.Stdout.WriteString("\n")
+	} else {
+		Printfln("Error: %v %v", err1.Error(), err2.Error())
 	}
 }
