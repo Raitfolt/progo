2155cabd81e7d8ae4028b90ea9a35467cd38eb22
Anton Novoseltcev <raitfolt@gmain.com>
2023-01-11 16:44:59 +0500

    htmltext allTemplates

--- htmltext/main.go
+++ htmltext/main.go
@@ -6,14 +6,13 @@ import (
 )
 
 func main() {
-	t1, err1 := template.ParseFiles("templates/template.html")
-	t2, err2 := template.ParseFiles("templates/extras.html")
-	if err1 == nil && err2 == nil {
-		t1.Execute(os.Stdout, &Kayak)
+	allTemplates, err := template.ParseFiles("templates/template.html", "templates/extras.html")
+	if err == nil {
+		allTemplates.ExecuteTemplate(os.Stdout, "template.html", &Kayak)
 		os.Stdout.WriteString("\n")
-		t2.Execute(os.Stdout, &Kayak)
+		allTemplates.ExecuteTemplate(os.Stdout, "extras.html", &Kayak)
 		os.Stdout.WriteString("\n")
 	} else {
-		Printfln("Error: %v %v", err1.Error(), err2.Error())
+		Printfln("Error: %v", err.Error())
 	}
 }
