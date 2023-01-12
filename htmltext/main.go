f0cb2d0093b30643d152890daef7cd634fc1b5f8
Антон Новосельцев <raitfolt@gmail.com>
2023-01-12 16:52:03 +0500

    781

--- htmltext/main.go
+++ htmltext/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"html/template"
 	"os"
-	"text/template"
+	"strings"
 )
+
+func GetCategories(products []Product) (categories []string) {
+	catMap := map[string]string{}
+	for _, p := range products {
+		if catMap[p.Category] == "" {
+			catMap[p.Category] = p.Category
+			categories = append(categories, p.Category)
+		}
+	}
+	return
+}
 
 func Exec(t *template.Template) error {
 	return t.Execute(os.Stdout, Products)
 }
 
 func main() {
-	allTemplates, err := template.ParseGlob("templates/*.html")
+	allTemplates := template.New("allTemplates")
+	allTemplates.Funcs(map[string]interface{}{
+		"getCats": GetCategories,
+		"lower":   strings.ToLower,
+	})
+	allTemplates, err := allTemplates.ParseGlob("templates/*.html")
 	if err == nil {
 		selectedTemplated := allTemplates.Lookup("mainTemplate")
 		err = Exec(selectedTemplated)
