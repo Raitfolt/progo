1acbbf3eecba678a4e803c1a39cb13f1d0d66f48
Антон Новосельцев <raitfolt@gmail.com>
2023-01-13 15:05:53 +0500

    httpserver favicon not found

--- httpserver/main.go
+++ httpserver/main.go
@@ -10,14 +10,12 @@ type StringHandler struct {
 }
 
 func (sh StringHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	Printfln("Method: %v", request.Method)
-	Printfln("URL: %v", request.URL)
-	Printfln("HTTP version: %v", request.Proto)
-	Printfln("Host: %v", request.Host)
-	for name, val := range request.Header {
-		Printfln("Header: %v, Value: %v", name, val)
+	if request.URL.Path == "/favicon.ico" {
+		Printfln("Request for icon detected - returning 404")
+		writer.WriteHeader(http.StatusNotFound)
+		return
 	}
-	Printfln("---")
+	Printfln("Request for %v", request.URL.Path)
 	io.WriteString(writer, sh.message)
 }
 
