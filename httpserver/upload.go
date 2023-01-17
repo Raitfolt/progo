0c5464609d14b18341b8d0ed247ff7228dbc6da2
Антон Новосельцев <raitfolt@gmail.com>
2023-01-17 13:24:52 +0500

    httpserver multipart files

--- httpserver/upload.go
+++ httpserver/upload.go
@@ -7,19 +7,21 @@ import (
 )
 
 func HandleMultipartForm(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Name: %v, City:%v\n", request.FormValue("name"), request.FormValue("city"))
-	fmt.Fprintln(writer, "------")
-	file, header, err := request.FormFile("files")
-	if err == nil {
-		defer file.Close()
-		fmt.Fprintf(writer, "Name: %v, Size: %v\n", header.Filename, header.Size)
-		for k, v := range header.Header {
-			fmt.Fprintf(writer, "Key %v, Value %v\n", k, v)
+	request.ParseMultipartForm(10000000)
+	fmt.Fprintf(writer, "Name: %v, City: %v\n",
+		request.MultipartForm.Value["name"][0],
+		request.MultipartForm.Value["name"][0])
+	fmt.Fprintf(writer, "------------------------------------------------------------")
+	for _, header := range request.MultipartForm.File["files"] {
+		fmt.Fprintf(writer, "Name: %v, Size %v\n", header.Filename, header.Size)
+		file, err := header.Open()
+		if err == nil {
+			defer file.Close()
+			fmt.Fprintln(writer, "------------------------------------------------------------")
+			io.Copy(writer, file)
+		} else {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
 		}
-		fmt.Fprintln(writer, "------")
-		io.Copy(writer, file)
-	} else {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
 	}
 }
 
