f4cc266730142a6279650c2c411ca5df09e1de12
Антон Новосельцев <raitfolt@gmail.com>
2023-01-17 16:51:27 +0500

    httpclient post method

--- httpclient/main.go
+++ httpclient/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"encoding/json"
+	"io"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -10,18 +11,17 @@ func main() {
 	go http.ListenAndServe(":5000", nil)
 	time.Sleep(time.Second)
 
-	response, err := http.Get("http://localhost:5000/json")
+	formData := map[string][]string{
+		"name":     {"Kayak"},
+		"category": {"Watersports"},
+		"price":    {"279"},
+	}
+
+	response, err := http.PostForm("http://localhost:5000/echo", formData)
+
 	if err == nil && response.StatusCode == http.StatusOK {
+		io.Copy(os.Stdout, response.Body)
 		defer response.Body.Close()
-		data := []Product{}
-		err = json.NewDecoder(response.Body).Decode(&data)
-		if err == nil {
-			for _, p := range data {
-				Printfln("Name: %v, Price: $%.2f", p.Name, p.Price)
-			}
-		} else {
-			Printfln("Decode error: %v", err.Error())
-		}
 	} else {
 		Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
 	}
