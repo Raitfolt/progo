e4bad5636b77dcb5790b56d4cafe1c67ae355cfc
Антон Новосельцев <raitfolt@gmail.com>
2023-02-02 11:14:28 +0500

    reflection work

--- reflection/main.go
+++ reflection/main.go
@@ -14,29 +14,36 @@ func getTypePath(t reflect.Type) (path string) {
 
 func printDetails(values ...interface{}) {
 	for _, elem := range values {
-		elemType := reflect.TypeOf(elem)
-		Printfln("Name: %v, PkgPath: %v, Kind: %v",
-			elemType.Name(), getTypePath(elemType), elemType.Kind())
+		//elemType := reflect.TypeOf(elem)
+		elemValue := reflect.ValueOf(elem)
+		switch elemValue.Kind() {
+		case reflect.Bool:
+			var val bool = elemValue.Bool()
+			Printfln("Bool: %v", val)
+		case reflect.Int:
+			var val int64 = elemValue.Int()
+			Printfln("Int: %v ", val)
+		case reflect.Float32, reflect.Float64:
+			var val float64 = elemValue.Float()
+			Printfln("Float: %v", val)
+		case reflect.String:
+			var val string = elemValue.String()
+			Printfln("String: %v", val)
+		case reflect.Ptr:
+			var val reflect.Value = elemValue.Elem()
+			if val.Kind() == reflect.Int {
+				Printfln("Pointer to Int: %v", val.Int())
+			}
+		default:
+			Printfln("Other: %v", elemValue.String())
+		}
 	}
-}
-
-type Payment struct {
-	Currency string
-	Amount   float64
-}
-
-type Payment2 struct {
-	Payment
-	Product
-	Customer
 }
 
 func main() {
 	product := Product{
 		Name: "Kayak", Category: "Watersports", Price: 279,
 	}
-	customer := Customer{Name: "Alice", City: "New York"}
-	payment := Payment{Currency: "USD", Amount: 100.50}
-	p2 := Payment2{Payment: payment, Product: product, Customer: customer}
-	printDetails(product, customer, payment, 10, true, p2)
+	number := 100
+	printDetails(true, 10, 23.30, "Alice", &number, product)
 }
