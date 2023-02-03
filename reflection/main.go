9f6d34ac5790427dbb60166fb66f01efd04f3fdc
Антон Новосельцев <raitfolt@gmail.com>
2023-02-03 14:23:21 +0500

    reflection overflow

--- reflection/main.go
+++ reflection/main.go
@@ -4,10 +4,35 @@ import (
 	"reflect"
 )
 
+func IsInt(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return true
+	}
+	return false
+}
+
+func IsFloat(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Float32, reflect.Float64:
+		return true
+	}
+	return false
+}
+
 func convert(src, target interface{}) (result interface{}, assigned bool) {
 	srcVal := reflect.ValueOf(src)
 	targetVal := reflect.ValueOf(target)
 	if srcVal.Type().ConvertibleTo(targetVal.Type()) {
+		if IsInt(targetVal) && IsInt(srcVal) &&
+			targetVal.OverflowInt(srcVal.Int()) {
+			Printfln("Int overflow")
+			return src, false
+		} else if IsFloat(targetVal) && IsFloat(srcVal) &&
+			targetVal.OverflowFloat(srcVal.Float()) {
+			Printfln("Float overflow")
+			return src, false
+		}
 		result = srcVal.Convert(targetVal.Type()).Interface()
 		assigned = true
 	} else {
@@ -23,5 +48,8 @@ func main() {
 	newVal, ok := convert(price, 100.00)
 	Printfln("Converted %v: %v, %T", ok, newVal, newVal)
 	newVal, ok = convert(name, 100.00)
+	Printfln("Converted %v: %v, %T", ok, newVal, newVal)
+
+	newVal, ok = convert(5000, int8(100))
 	Printfln("Converted %v: %v, %T", ok, newVal, newVal)
 }
