1f18ac8464c9445db0801ed4a73ce0662dcfa782
Антон Новосельцев <raitfolt@gmail.com>
2023-02-06 10:20:31 +0500

    reflection 935

--- reflection/main.go
+++ reflection/main.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"reflect"
+	"strings"
 )
 
-func createPointerType(t reflect.Type) reflect.Type {
-	return reflect.PtrTo(t)
-}
+var stringPtrType = reflect.TypeOf((*string)(nil))
 
-func followPointerType(t reflect.Type) reflect.Type {
-	if t.Kind() == reflect.Ptr {
-		return t.Elem()
+func transformString(val interface{}) {
+	elemValue := reflect.ValueOf(val)
+	if elemValue.Type() == stringPtrType {
+		upperStr := strings.ToUpper(elemValue.Elem().String())
+		if elemValue.Elem().CanSet() {
+			elemValue.Elem().SetString(upperStr)
+		}
 	}
-	return t
 }
 
 func main() {
 	name := "Alice"
 
-	t := reflect.TypeOf(name)
-	Printfln("Original Type: %v", t)
-	pt := createPointerType(t)
-	Printfln("Pointer Type: %v", pt)
-	Printfln("Follow pointer type: %v", followPointerType(pt))
+	transformString(&name)
+	Printfln("Follow pointer value: %v", name)
 }
