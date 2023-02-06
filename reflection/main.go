5982b06f1d53620bdc06724ca334538b588ddc6d
Антон Новосельцев <raitfolt@gmail.com>
2023-02-06 11:32:16 +0500

    reflection 937

--- reflection/main.go
+++ reflection/main.go
@@ -4,12 +4,19 @@ import (
 	"reflect"
 )
 
-func checkElemType(val interface{}, arrOrSlice interface{}) bool {
-	elemType := reflect.TypeOf(val)
-	arrOrSliceType := reflect.TypeOf(arrOrSlice)
-	return (arrOrSliceType.Kind() == reflect.Array ||
-		arrOrSliceType.Kind() == reflect.Slice) &&
-		arrOrSliceType.Elem() == elemType
+func setValue(arrayOrSlice interface{}, index int, replacment interface{}) {
+	arrayOrSliceVal := reflect.ValueOf(arrayOrSlice)
+	replacementVal := reflect.ValueOf(replacment)
+	if arrayOrSliceVal.Kind() == reflect.Slice {
+		elemVal := arrayOrSliceVal.Index(index)
+		if elemVal.CanSet() {
+			elemVal.Set(replacementVal)
+		}
+	} else if arrayOrSliceVal.Kind() == reflect.Ptr &&
+		arrayOrSliceVal.Elem().Kind() == reflect.Array &&
+		arrayOrSliceVal.Elem().CanSet() {
+		arrayOrSliceVal.Elem().Index(index).Set(replacementVal)
+	}
 }
 
 func main() {
@@ -20,7 +27,13 @@ func main() {
 	slice := []string{name, city, hobby}
 	array := [3]string{name, city, hobby}
 
-	Printfln("Slice (string): %v", checkElemType("testString", slice))
-	Printfln("Array (string): %v", checkElemType("testString", array))
-	Printfln("Array (int): %v", checkElemType(10, array))
+	Printfln("Original slice: %v", slice)
+	newCity := "Paris"
+	setValue(slice, 1, newCity)
+	Printfln("Modyfied slice: %v", slice)
+
+	Printfln("Original slice: %v", array)
+	newCity = "Rome"
+	setValue(&array, 1, newCity)
+	Printfln("Modyfied slice: %v", array)
 }
