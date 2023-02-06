572a88a83f87d644b76169ac90a8cca763baf6a7
Антон Новосельцев <raitfolt@gmail.com>
2023-02-06 14:25:33 +0500

    reflection 941

--- reflection/main.go
+++ reflection/main.go
@@ -4,15 +4,17 @@ import (
 	"reflect"
 )
 
-func enumerateStrings(arrayOrSlice interface{}) {
-	arrayOrSliceVal := reflect.ValueOf(arrayOrSlice)
-	if (arrayOrSliceVal.Kind() == reflect.Array ||
-		arrayOrSliceVal.Kind() == reflect.Slice) &&
-		arrayOrSliceVal.Type().Elem().Kind() == reflect.String {
-		for i := 0; i < arrayOrSliceVal.Len(); i++ {
-			Printfln("Element: %v, Value: %v", i, arrayOrSliceVal.Index(i).String())
+func findAndSplit(slice interface{}, target interface{}) interface{} {
+	sliceVal := reflect.ValueOf(slice)
+	targetType := reflect.TypeOf(target)
+	if sliceVal.Kind() == reflect.Slice && sliceVal.Type().Elem() == targetType {
+		for i := 0; i < sliceVal.Len(); i++ {
+			if sliceVal.Index(i).Interface() == target {
+				return sliceVal.Slice(0, i+1)
+			}
 		}
 	}
+	return slice
 }
 
 func main() {
@@ -21,8 +23,8 @@ func main() {
 	hobby := "Running"
 
 	slice := []string{name, city, hobby}
-	array := [3]string{name, city, hobby}
+	Printfln("Strings: %v", findAndSplit(slice, "London"))
 
-	enumerateStrings(slice)
-	enumerateStrings(array)
+	numbers := []int{1, 3, 5, 7, 9}
+	Printfln("Numbers: %v", findAndSplit(numbers, 4))
 }
