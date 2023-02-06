567942c382a7e4a28c8c88e6a3c706117c08871c
Антон Новосельцев <raitfolt@gmail.com>
2023-02-06 16:38:06 +0500

    reflection 953

--- reflection/main.go
+++ reflection/main.go
@@ -2,56 +2,26 @@ package main
 
 import (
 	"reflect"
-	"strings"
 )
 
-func setMap(m interface{}, key interface{}, val interface{}) {
-	mapValue := reflect.ValueOf(m)
-	keyValue := reflect.ValueOf(key)
-	valValue := reflect.ValueOf(val)
-	if mapValue.Kind() == reflect.Map &&
-		mapValue.Type().Key() == keyValue.Type() &&
-		mapValue.Type().Elem() == valValue.Type() {
-		mapValue.SetMapIndex(keyValue, valValue)
-	} else {
-		Printfln("Not a map or mosmatched types")
-	}
-}
-
-func removeFromMap(m interface{}, key interface{}) {
-	mapValue := reflect.ValueOf(m)
-	keyValue := reflect.ValueOf(key)
-	if mapValue.Kind() == reflect.Map &&
-		mapValue.Type().Key() == keyValue.Type() {
-		mapValue.SetMapIndex(keyValue, reflect.Value{})
-	}
-}
-
-func createMap(slice interface{}, op func(interface{}) interface{}) interface{} {
-	sliceVal := reflect.ValueOf(slice)
-	if sliceVal.Kind() == reflect.Slice {
-		mapType := reflect.MapOf(sliceVal.Type().Elem(), sliceVal.Type().Elem())
-		mapVal := reflect.MakeMap(mapType)
-		for i := 0; i < sliceVal.Len(); i++ {
-			elemVal := sliceVal.Index(i)
-			mapVal.SetMapIndex(elemVal, reflect.ValueOf(op(elemVal.Interface())))
+func inspectStructs(structs ...interface{}) {
+	for _, s := range structs {
+		structType := reflect.TypeOf(s)
+		if structType.Kind() == reflect.Struct {
+			inspectStructType(structType)
 		}
-		return mapVal.Interface()
 	}
-	return nil
+}
+
+func inspectStructType(structType reflect.Type) {
+	Printfln("--- Struct Type: %v", structType)
+	for i := 0; i < structType.NumField(); i++ {
+		field := structType.Field(i)
+		Printfln("Field: %v Name: %v, Type:%v, Exported: %v", field.Index, field.Name, field.Type, field.PkgPath == "")
+	}
+	Printfln("--- End Struct Type: %v", structType)
 }
 
 func main() {
-	names := []string{"Alice", "Bob", "Chrlie"}
-	reverse := func(val interface{}) interface{} {
-		if str, ok := val.(string); ok {
-			return strings.ToUpper(str)
-		}
-		return val
-	}
-
-	namesMap := createMap(names, reverse).(map[string]string)
-	for k, v := range namesMap {
-		Printfln("Key: %v, Value: %v", k, v)
-	}
+	inspectStructs(Purchase{})
 }
