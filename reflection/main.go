96b8704620af3ba90cc8dd21e6e0a77f11ede3bf
Антон Новосельцев <raitfolt@gmail.com>
2023-02-07 13:35:09 +0500

    reflection 957

--- reflection/main.go
+++ reflection/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 )
 
@@ -27,6 +28,25 @@ func inspectStructType(baseIndex []int, structType reflect.Type) {
 	Printfln("--- End Struct Type: %v", structType)
 }
 
+func describeField(s interface{}, fieldName string) {
+	structType := reflect.TypeOf(s)
+	field, found := structType.FieldByName(fieldName)
+	if found {
+		Printfln("Found: %v, Type: %v, Index: %v", field.Name, field.Type, field.Index)
+		index := field.Index
+		for len(index) > 1 {
+			index = index[0 : len(index)-1]
+			field = structType.FieldByIndex(index)
+			Printfln("Parent: %v, Type: %v, Index: %v", field.Name, field.Type, field.Index)
+		}
+		Printfln("Top-Level Type: %v", structType)
+	} else {
+		Printfln("Field %v not found", fieldName)
+	}
+}
+
 func main() {
 	inspectStructs(Purchase{})
+	fmt.Println("-----------------------------------------------------")
+	describeField(Purchase{}, "Price")
 }
