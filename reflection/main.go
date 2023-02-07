73e337ac4569652109489043c62625a5cdbf5245
Антон Новосельцев <raitfolt@gmail.com>
2023-02-07 14:45:35 +0500

    reflection 959

--- reflection/main.go
+++ reflection/main.go
@@ -1,49 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"reflect"
 )
-
-func inspectStructs(structs ...interface{}) {
-	for _, s := range structs {
-		structType := reflect.TypeOf(s)
-		if structType.Kind() == reflect.Struct {
-			inspectStructType([]int{}, structType)
-		}
-	}
-}
-
-func inspectStructType(baseIndex []int, structType reflect.Type) {
-	Printfln("--- Struct Type: %v", structType)
-	for i := 0; i < structType.NumField(); i++ {
-		fieldIndex := append(baseIndex, i)
-		field := structType.Field(i)
-		Printfln("Field: %v Name: %v, Type:%v, Exported: %v", field.Index, field.Name, field.Type, field.PkgPath == "")
-		if field.Type.Kind() == reflect.Struct {
-			field := structType.FieldByIndex(field.Index)
-			inspectStructType(fieldIndex, field.Type)
-		}
-	}
-	Printfln("--- End Struct Type: %v", structType)
-}
-
-func describeField(s interface{}, fieldName string) {
-	structType := reflect.TypeOf(s)
-	field, found := structType.FieldByName(fieldName)
-	if found {
-		Printfln("Found: %v, Type: %v, Index: %v", field.Name, field.Type, field.Index)
-		index := field.Index
-		for len(index) > 1 {
-			index = index[0 : len(index)-1]
-			field = structType.FieldByIndex(index)
-			Printfln("Parent: %v, Type: %v, Index: %v", field.Name, field.Type, field.Index)
-		}
-		Printfln("Top-Level Type: %v", structType)
-	} else {
-		Printfln("Field %v not found", fieldName)
-	}
-}
 
 func inspectTags(s interface{}, tagName string) {
 	structType := reflect.TypeOf(s)
@@ -57,16 +16,14 @@ func inspectTags(s interface{}, tagName string) {
 	}
 }
 
-type Person struct {
-	Name    string `alias:"id"`
-	City    string `alias:""`
-	Country string
-}
-
 func main() {
-	inspectStructs(Purchase{})
-	fmt.Println("-----------------------------------------------------")
-	describeField(Purchase{}, "Price")
-	fmt.Println("-----------------------------------------------------")
-	inspectTags(Person{}, "alias")
+	stringType := reflect.TypeOf("string")
+
+	structType := reflect.StructOf([]reflect.StructField{
+		{Name: "Name", Type: stringType, Tag: `alias:"id"`},
+		{Name: "City", Type: stringType, Tag: `alias:""`},
+		{Name: "Country", Type: stringType},
+	})
+
+	inspectTags(reflect.New(structType), "alias")
 }
