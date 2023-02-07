34c8d18fa7321992aa16a1566114e278cc9c3162
Антон Новосельцев <raitfolt@gmail.com>
2023-02-07 14:18:08 +0500

    reflection 959

--- reflection/main.go
+++ reflection/main.go
@@ -45,8 +45,28 @@ func describeField(s interface{}, fieldName string) {
 	}
 }
 
+func inspectTags(s interface{}, tagName string) {
+	structType := reflect.TypeOf(s)
+	for i := 0; i < structType.NumField(); i++ {
+		field := structType.Field(i)
+		tag := field.Tag
+		valGet := tag.Get(tagName)
+		valLookup, ok := tag.Lookup(tagName)
+		Printfln("Field: %v, Tag %v: %v", field.Name, tagName, valGet)
+		Printfln("Field: %v, Tag %v: %v, Set: %v", field.Name, tagName, valLookup, ok)
+	}
+}
+
+type Person struct {
+	Name    string `alias:"id"`
+	City    string `alias:""`
+	Country string
+}
+
 func main() {
 	inspectStructs(Purchase{})
 	fmt.Println("-----------------------------------------------------")
 	describeField(Purchase{}, "Price")
+	fmt.Println("-----------------------------------------------------")
+	inspectTags(Person{}, "alias")
 }
