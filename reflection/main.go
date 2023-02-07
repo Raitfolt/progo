eb86ad179935f8c11a43b017ab2b9dc6f46d5aa2
Антон Новосельцев <raitfolt@gmail.com>
2023-02-07 16:15:59 +0500

    reflection 970

--- reflection/main.go
+++ reflection/main.go
@@ -24,6 +24,21 @@ func inspectFuncType(f interface{}) {
 	}
 }
 
+func invokeFunction(f interface{}, params ...interface{}) {
+	paramVals := []reflect.Value{}
+	for _, p := range params {
+		paramVals = append(paramVals, reflect.ValueOf(p))
+	}
+	funcVal := reflect.ValueOf(f)
+	if funcVal.Kind() == reflect.Func {
+		results := funcVal.Call(paramVals)
+		for i, r := range results {
+			Printfln("Result #%v: %v", i, r)
+		}
+	}
+}
+
 func main() {
-	inspectFuncType(Find)
+	names := []string{"Alice", "Bob", "Charlie"}
+	invokeFunction(Find, names, "London", "Bob")
 }
