604d9e76f038539413246000356dacd5fe982926
Антон Новосельцев <raitfolt@gmail.com>
2023-02-07 16:24:05 +0500

    reflection 972

--- reflection/main.go
+++ reflection/main.go
@@ -2,43 +2,28 @@ package main
 
 import (
 	"reflect"
+	"strings"
 )
 
-func inspectFuncType(f interface{}) {
-	funcType := reflect.TypeOf(f)
-	if funcType.Kind() == reflect.Func {
-		Printfln("Function parameters: %v", funcType.NumIn())
-		for i := 0; i < funcType.NumIn(); i++ {
-			paramType := funcType.In(i)
-			if i < funcType.NumIn()-1 {
-				Printfln("Parameter #%v, Type: %v", i, paramType)
-			} else {
-				Printfln("Parameter #%v, Type: %v, Variadic: %v", i, paramType, funcType.IsVariadic())
+func mapSlice(slice interface{}, mapper interface{}) (mapped []interface{}) {
+	sliceVal := reflect.ValueOf(slice)
+	mapperVal := reflect.ValueOf(mapper)
+	mapped = []interface{}{}
+	if sliceVal.Kind() == reflect.Slice && mapperVal.Kind() == reflect.Func &&
+		mapperVal.Type().NumIn() == 1 &&
+		mapperVal.Type().In(0) == sliceVal.Type().Elem() {
+		for i := 0; i < sliceVal.Len(); i++ {
+			result := mapperVal.Call([]reflect.Value{sliceVal.Index(i)})
+			for _, r := range result {
+				mapped = append(mapped, r.Interface())
 			}
 		}
-		Printfln("Function results: %v", funcType.NumOut())
-		for i := 0; i < funcType.NumOut(); i++ {
-			resultType := funcType.Out(i)
-			Printfln("Result #%v, Type: %v", i, resultType)
-		}
 	}
-}
-
-func invokeFunction(f interface{}, params ...interface{}) {
-	paramVals := []reflect.Value{}
-	for _, p := range params {
-		paramVals = append(paramVals, reflect.ValueOf(p))
-	}
-	funcVal := reflect.ValueOf(f)
-	if funcVal.Kind() == reflect.Func {
-		results := funcVal.Call(paramVals)
-		for i, r := range results {
-			Printfln("Result #%v: %v", i, r)
-		}
-	}
+	return
 }
 
 func main() {
 	names := []string{"Alice", "Bob", "Charlie"}
-	invokeFunction(Find, names, "London", "Bob")
+	results := mapSlice(names, strings.ToUpper)
+	Printfln("Results: %v", results)
 }
