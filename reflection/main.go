82000310f44a27520fd48cdfe7299d3398d4862d
Антон Новосельцев <raitfolt@gmail.com>
2023-02-07 16:43:05 +0500

    reflection 976

--- reflection/main.go
+++ reflection/main.go
@@ -1,37 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"strings"
 )
 
-func mapSlice(slice interface{}, mapper interface{}) (mapped []interface{}) {
-	sliceVal := reflect.ValueOf(slice)
-	mapperVal := reflect.ValueOf(mapper)
-	mapped = []interface{}{}
-	if sliceVal.Kind() == reflect.Slice && mapperVal.Kind() == reflect.Func {
-		paramTypes := []reflect.Type{sliceVal.Type().Elem()}
-		resultTypes := []reflect.Type{}
-		for i := 0; i < mapperVal.Type().NumOut(); i++ {
-			resultTypes = append(resultTypes, mapperVal.Type().Out(i))
-		}
-		expectedFuncType := reflect.FuncOf(paramTypes, resultTypes, mapperVal.Type().IsVariadic())
-		if mapperVal.Type() == expectedFuncType {
-			for i := 0; i < sliceVal.Len(); i++ {
-				result := mapperVal.Call([]reflect.Value{sliceVal.Index(i)})
-				for _, r := range result {
-					mapped = append(mapped, r.Interface())
+func makeMapperFunc(mapper interface{}) interface{} {
+	mapVal := reflect.ValueOf(mapper)
+	if mapVal.Kind() == reflect.Func && mapVal.Type().NumIn() == 1 &&
+		mapVal.Type().NumOut() == 1 {
+		inType := reflect.SliceOf(mapVal.Type().In(0))
+		inTypeSlice := []reflect.Type{inType}
+		outType := reflect.SliceOf(mapVal.Type().Out(0))
+		outTypeSlice := []reflect.Type{outType}
+		funcType := reflect.FuncOf(inTypeSlice, outTypeSlice,
+			false)
+		funcVal := reflect.MakeFunc(funcType,
+			func(params []reflect.Value) (results []reflect.Value) {
+				srcSliceVal := params[0]
+				resultsSliceVal := reflect.MakeSlice(outType, srcSliceVal.Len(), 10)
+				for i := 0; i < srcSliceVal.Len(); i++ {
+					r := mapVal.Call([]reflect.Value{
+						srcSliceVal.Index(i)})
+					resultsSliceVal.Index(i).Set(r[0])
 				}
-			}
-		} else {
-			Printfln("Function type not as expected")
-		}
+				results = []reflect.Value{resultsSliceVal}
+				return
+			})
+		return funcVal.Interface()
 	}
-	return
+	Printfln("Unexpected types")
+	return nil
 }
-
 func main() {
+	lowerStringMapper := makeMapperFunc(strings.ToLower).(func([]string) []string)
 	names := []string{"Alice", "Bob", "Charlie"}
-	results := mapSlice(names, strings.ToUpper)
-	Printfln("Results: %v", results)
+	results := lowerStringMapper(names)
+	Printfln("Lowercase Results: %v", results)
+
+	incrementFloatMapper := makeMapperFunc(func(val float64) float64 {
+		return val + 1
+	}).(func([]float64) []float64)
+	prices := []float64{279, 48.95, 19.50}
+	floatResults := incrementFloatMapper(prices)
+	Printfln("Increment Results: %v", floatResults)
+	floatToStringMapper := makeMapperFunc(func(val float64) string {
+		return fmt.Sprintf("$%.2f", val)
+	}).(func([]float64) []string)
+	Printfln("Price Results: %v",
+		floatToStringMapper(prices))
 }
