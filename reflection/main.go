99d62c45ccb29a892b8bc68f579038cf7718de89
Антон Новосельцев <raitfolt@gmail.com>
2023-02-07 13:29:13 +0500

    reflection 955

--- reflection/main.go
+++ reflection/main.go
@@ -8,16 +8,21 @@ func inspectStructs(structs ...interface{}) {
 	for _, s := range structs {
 		structType := reflect.TypeOf(s)
 		if structType.Kind() == reflect.Struct {
-			inspectStructType(structType)
+			inspectStructType([]int{}, structType)
 		}
 	}
 }
 
-func inspectStructType(structType reflect.Type) {
+func inspectStructType(baseIndex []int, structType reflect.Type) {
 	Printfln("--- Struct Type: %v", structType)
 	for i := 0; i < structType.NumField(); i++ {
+		fieldIndex := append(baseIndex, i)
 		field := structType.Field(i)
 		Printfln("Field: %v Name: %v, Type:%v, Exported: %v", field.Index, field.Name, field.Type, field.PkgPath == "")
+		if field.Type.Kind() == reflect.Struct {
+			field := structType.FieldByIndex(field.Index)
+			inspectStructType(fieldIndex, field.Type)
+		}
 	}
 	Printfln("--- End Struct Type: %v", structType)
 }
