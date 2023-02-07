72178e00b3cc55c5654388d71a4b654ea03dbe58
Антон Новосельцев <raitfolt@gmail.com>
2023-02-07 15:03:33 +0500

    reflection 963

--- reflection/main.go
+++ reflection/main.go
@@ -17,9 +17,37 @@ func getFieldValues(s interface{}) {
 	}
 }
 
+func setFieldValue(s interface{}, newVals map[string]interface{}) {
+	structValue := reflect.ValueOf(s)
+	if structValue.Kind() == reflect.Ptr && structValue.Elem().Kind() == reflect.Struct {
+		for name, newValue := range newVals {
+			fieldVal := structValue.Elem().FieldByName(name)
+			if fieldVal.CanSet() {
+				fieldVal.Set(reflect.ValueOf(newValue))
+			} else if fieldVal.CanAddr() {
+				ptr := fieldVal.Addr()
+				if ptr.CanSet() {
+					ptr.Set(reflect.ValueOf(newValue))
+				} else {
+					Printfln("Cannot set field via pointer")
+				}
+			} else {
+				Printfln("Cannot set field")
+			}
+		}
+	} else {
+		Printfln("Not a pointer to a struct")
+	}
+}
+
 func main() {
 	product := Product{Name: "Kayak", Category: "Watersports", Price: 279}
 	customer := Customer{Name: "Acme", City: "Chicago"}
 	purchase := Purchase{Customer: customer, Product: product, Total: 279, taxRate: 10}
+
+	setFieldValue(&purchase, map[string]interface{}{
+		"City": "London", "Category": "Boats", "Total": 100.50,
+	})
+
 	getFieldValues(purchase)
 }
