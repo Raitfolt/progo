8833cabe16bcb98bf450c7f102d0e3e4cba1a9ee
Антон Новосельцев <raitfolt@gmail.com>
2023-02-08 10:42:22 +0500

    coordination mutex

--- coordination/main.go
+++ coordination/main.go
@@ -1,21 +1,31 @@
 package main
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 var waitGroup = sync.WaitGroup{}
+var mutex = sync.Mutex{}
 
 func doSum(count int, val *int) {
+	time.Sleep(time.Second)
+	mutex.Lock()
 	for i := 0; i < count; i++ {
 		*val++
 	}
+	mutex.Unlock()
 	waitGroup.Done()
 }
 
 func main() {
 	counter := 0
 
-	waitGroup.Add(1)
-	go doSum(5000, &counter)
+	numRoutines := 5
+	waitGroup.Add(numRoutines)
+	for i := 0; i < numRoutines; i++ {
+		go doSum(10000, &counter)
+	}
 	waitGroup.Wait()
 
 	Printfln("Total: %v", counter)
