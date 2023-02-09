6bac28b0237ee8564c75d5fb9933deb14f9d6294
Антон Новосельцев <raitfolt@gmail.com>
2023-02-09 16:31:41 +0500

    platform wtf is this?

--- platform/main.go
+++ platform/main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"platform/config"
 	"platform/logging"
+	"platform/services"
 )
 
-func writeMessage(logger logging.Logger) {
-	logger.Info("Hello, Platform")
+func writeMessage(logger logging.Logger, cfg config.Configuration) {
+	section, ok := cfg.GetSection("main")
+	if ok {
+		message, ok := section.GetString("message")
+		if ok {
+			logger.Info(message)
+		} else {
+			logger.Panic("Cannot find configuration setting")
+		}
+	} else {
+		logger.Panic("Config section not found")
+	}
 }
 
 func main() {
-	var logger logging.Logger = logging.NewDefaultLogger(logging.Information)
-	writeMessage(logger)
+	services.RegisterDefaultServices()
+
+	services.Call(writeMessage)
+
+	val := struct {
+		message string
+		logging.Logger
+	}{
+		message: "Hello from the struct",
+	}
+	services.Populate(&val)
+	val.Logger.Debug(val.message)
 }
